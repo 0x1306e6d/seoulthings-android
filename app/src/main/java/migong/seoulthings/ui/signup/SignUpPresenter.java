57737b2958ace33088114c64216d945b0359b1b2
Gihwan Kim <ghkim3221@gmail.com>
2018-08-24 18:57:18 +0900

    Firebase Password Authentication 회원가입 기능 구현

--- app/src/main/java/migong/seoulthings/ui/signup/SignUpPresenter.java
+++ app/src/main/java/migong/seoulthings/ui/signup/SignUpPresenter.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import migong.seoulthings.ui.Presenter;
+import migong.seoulthings.util.AuthenticationUtils;
 
 public class SignUpPresenter implements Presenter {
 
   private static final String TAG = SignUpPresenter.class.getSimpleName();
+
+  private FirebaseAuth mAuth;
 
   @NonNull
   private final SignUpView mView;
@@ -20,6 +27,8 @@ public class SignUpPresenter implements Presenter {
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
+
+    mAuth = FirebaseAuth.getInstance();
   }
 
   @Override
@@ -38,10 +47,35 @@ public class SignUpPresenter implements Presenter {
   }
 
   public void onSignUpButtonClicked() {
-    // TODO(@gihwan): 회원가입 처리
+    final String email = mView.getEmail();
+    final String password = mView.getPassword();
+
+    if (!AuthenticationUtils.isValidEmailAddress(email)) {
+      mView.showValidEmailInputRequest();
+      return;
+    }
+
+    if (!AuthenticationUtils.isValidPassword(password)) {
+      mView.showValidPasswordInputRequest();
+      return;
+    }
+
+    mAuth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this::completeSignUp);
   }
 
   public void onSignInButtonClicked() {
     mView.startSignInActivity();
+  }
+
+  private void completeSignUp(@NonNull Task<AuthResult> task) {
+    if (task.isSuccessful()) {
+      final FirebaseUser user = mAuth.getCurrentUser();
+
+      Log.d(TAG, "completeSignUp: success, user=" + user);
+    } else {
+      mView.showSignUpFailure();
+      Log.w(TAG, "completeSignUp: failure", task.getException());
+    }
   }
 }

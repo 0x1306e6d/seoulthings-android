fe1c5927e9cb2030f4b32077eb46619ae64d2c0d
Gihwan Kim <ghkim3221@gmail.com>
2018-09-02 20:18:55 +0900

    MainActivity에 BottomNavigation 추가

--- app/src/main/java/migong/seoulthings/ui/main/MainActivity.java
+++ app/src/main/java/migong/seoulthings/ui/main/MainActivity.java
@@ -1,10 +1,13 @@
 package migong.seoulthings.ui.main;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import migong.seoulthings.R;
 
 public class MainActivity extends AppCompatActivity implements MainView {
+
+  private BottomNavigationView mBottomNavigation;
 
   private MainPresenter mPresenter;
 
@@ -12,6 +15,15 @@ public class MainActivity extends AppCompatActivity implements MainView {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main_activity);
+
+    mBottomNavigation = findViewById(R.id.main_bottom_navigation);
+    mBottomNavigation.setOnNavigationItemSelectedListener(item -> {
+      // 선택되지 않은 메뉴를 선택할 경우에만 동작 수행.
+      return mPresenter.onBottomNavigationItemSelected(item);
+    });
+    mBottomNavigation.setOnNavigationItemReselectedListener(item -> {
+      // 선택된 메뉴를 다시 선택할 경우 아무런 동작도 수행하지 않음.
+    });
 
     mPresenter = new MainPresenter(this);
     mPresenter.onCreate(savedInstanceState);
--- app/src/main/java/migong/seoulthings/ui/main/MainPresenter.java
+++ app/src/main/java/migong/seoulthings/ui/main/MainPresenter.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.MenuItem;
 import migong.seoulthings.ui.Presenter;
 
 public class MainPresenter implements Presenter {
@@ -35,5 +36,13 @@ public class MainPresenter implements Presenter {
   @Override
   public void onDestroy() {
     Log.d(TAG, "onDestroy() called");
+  }
+
+  public boolean onBottomNavigationItemSelected(@NonNull MenuItem item) {
+    Log.d(TAG, "onBottomNavigationItemSelected() called with: item = [" + item + "]");
+
+    // TODO(@gihwan): replace fragment
+
+    return true;
   }
 }

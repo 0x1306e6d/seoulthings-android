657dc47327abd866fbe724b5f65aa77aea0ba1dd
Gihwan Kim <ghkim3221@gmail.com>
2018-09-25 16:59:27 +0900

    Heroku 사용

--- app/src/main/java/migong/seoulthings/SeoulThingsConstants.java
+++ app/src/main/java/migong/seoulthings/SeoulThingsConstants.java
@@ -7,7 +7,7 @@ public final class SeoulThingsConstants {
   public static final int PASSWORD_MIN_LENGTH = 4; // TODO(@gihwan): 8로 설정. 현재 테스트용
   public static final int PASSWORD_MAX_LENGTH = 24;
 
-  public static final String SEOULTHINGS_SERVER_BASE_URL = "http://172.30.1.11:8080";
+  public static final String SEOULTHINGS_SERVER_BASE_URL = "https://seoulthings.herokuapp.com";
 
   private SeoulThingsConstants() {
     // nothing
